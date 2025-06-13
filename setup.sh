851d7d4064435d25fa7d5963e1ffc1efd50eea67
Nikita Naumov <neknaum1@gmail.com>
2025-06-13 16:16:53 +0300

    Update dependencies. Add tmux vi mode. Add plugins

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo $distro | grep "Ubuntu"
 if [[ $? -eq 0 ]]; then
     sudo add-apt-repository ppa:neovim-ppa/unstable -y
     sudo apt-get update
-    sudo apt-get install -y tmux make gcc ripgrep unzip git xclip neovim
+    sudo apt-get install -y tmux make gcc ripgrep unzip git xclip neovim clangd
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
     mkdir -p ~/.config/tmux/
     git clone https://github.com/HabKaffee/kickstart.nvim.git ~/.config/nvim/
